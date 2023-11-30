c93c2488d0fdddc9e217253d70c3c297ec007b65
Mehmet Ismail <drinoff@gmail.com>
2023-11-30 17:47:43 +0200

    styling

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,25 +5,24 @@ import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import logo from './Assets/logo.svg';
+import './Navigation.css';
 
-const pages = ['За Нас','Уреди', 'Услуги', 'Контакти'];
+const pages = ['За Нас', 'Уреди', 'Услуги', 'Контакти'];
 
 function Navigation() {
-
   return (
-    <AppBar position="static" sx={{backgroundColor:'#FCFCFC'}}>
+    <AppBar position="static" sx={{ backgroundColor: '#FCFCFC' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          <img src={logo} alt="" />
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: '#4462C4', display: 'block', fontWeight: 'bold' }}
-              >
-                {page}
-              </Button>
-            ))}
+            <img src={logo} alt="" />
+            <div className="navLinks">
+              {pages.map((page) => (
+                <Button disableRipple key={page} sx={{ my: 2, color: '#4462C4', display: 'block', fontWeight: 'bold' }}>
+                  {page}
+                </Button>
+              ))}
+            </div>
           </Box>
         </Toolbar>
       </Container>
