33682a10c877f52009c692d32d065cdb02d36a7d
Mehmet Ismail <drinoff@gmail.com>
2023-11-30 18:26:15 +0200

    more styling for mobile

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,20 +2,73 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 import logo from './Assets/logo.svg';
 import './Navigation.css';
 
-const pages = ['За Нас', 'Уреди', 'Услуги', 'Контакти'];
+const pages = ['За Нас','Уреди', 'Услуги', 'Контакти'];
 
-function Navigation() {
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#FCFCFC' }}>
+    <AppBar position="static" sx={{backgroundColor:'#FCFCFC'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'space-between' }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+            <img className="mobileVersionLogo" src={logo} alt="logo" />
+          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <img src={logo} alt="" />
+            <img src={logo} alt="logo" />
             <div className="navLinks">
               {pages.map((page) => (
                 <Button disableRipple key={page} sx={{ my: 2, color: '#4462C4', display: 'block', fontWeight: 'bold' }}>
@@ -29,4 +82,5 @@ function Navigation() {
     </AppBar>
   );
 }
-export default Navigation;
+export default ResponsiveAppBar;
+
