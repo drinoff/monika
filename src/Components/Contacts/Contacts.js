f18de59848898a4b7ac66eaa5527e6b43d51eaae
Mehmet Ismail <drinoff@gmail.com>
2023-12-02 09:41:05 +0200

    Update Contacts.js

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer} from 'react-leaflet';
 import { Marker, Popup } from 'react-leaflet';
 import './Contacts.css';
 
