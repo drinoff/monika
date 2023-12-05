df0833569e1eff516fd73164912ba0f8684ca549
Mehmet Ismail <drinoff@gmail.com>
2023-12-05 18:53:16 +0200

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import './Home.css';
 import first from '../Assets/carousel/1.jpg';
